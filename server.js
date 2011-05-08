cad375ec3465e5bb158c8d383d0e9aef1b0556e9
Greg Allen <me@jga.me>
2011-05-07 18:42:29 -0700

    tabs to spaces

--- server.js
+++ server.js
@@ -9,101 +9,101 @@ var cwd = process.cwd();
 var build = "debug";
 var config = null;
 if (path.existsSync(cwd+"/config.js"))
-    config = require(cwd+"/config");
+  config = require(cwd+"/config");
 else
-    config = require(__dirname+"/config");
+  config = require(__dirname+"/config");
 
 var port = 3000;
 var generating = false;
 if (args["--generate"]) {
-	generating = true;
-	var b = args["--generate"];
-	if (typeof b === "string")
-		build = b;
-    port = 3003;
+  generating = true;
+  var b = args["--generate"];
+  if (typeof b === "string")
+    build = b;
+  port = 3003;
 }
 
 if (args["--startapp"]) {
-    var source = __dirname+"/structure";
-    var destination = cwd+"/";
-    require("child_process").spawn("cp", ['-r', source, destination]);
+  var source = __dirname+"/structure";
+  var destination = cwd+"/";
+  require("child_process").spawn("cp", ['-r', source, destination]);
 } else {
-    app.configure(function() {
-        app.use(express.methodOverride());
-        app.use(express.bodyParser());
-        app.use(app.router);
+  app.configure(function() {
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
 
-        app.set("views", cwd+"/templates");
-        app.set("view engine", config.settings.templateEngine);
-        app.set("view options", { layout: false, open: "{{", close: "}}" });
-
-
-        var stylusCompile = function(str, path) {
-            return stylus(str).set('filename', path).set('compress', true);
-        };
-
-        if (config.settings.useStylus) {
-            var stylus = require("stylus");
-            app.use("/ui", stylus.middleware({
-                src: cwd + '/ui',
-                dest: cwd + '/ui',
-                compile: stylusCompile
-            }));
-        }
-
-        var compiledDir = cwd+"/compiled";
-        app.use("/compiled", express.compiler({ src: cwd+"/app", dest: compiledDir, enable: ['less', 'coffeescript']}));
-        app.use("/compiled", express.static(compiledDir));
-
-        app.use("/ui", express.static(cwd+"/ui"));
-        app.use("/app", express.static(cwd+"/app"));
-
-        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    });
+    app.set("views", cwd+"/templates");
+    app.set("view engine", config.settings.templateEngine);
+    app.set("view options", { layout: false, open: "{{", close: "}}" });
 
 
-    app.get("/", function(req, res) { 
+    var stylusCompile = function(str, path) {
+      return stylus(str).set('filename', path).set('compress', true);
+    };
 
-        locals = { 
-            debug: false,
-            phonegap: true,
-            config: config[build],
-            weinre: function(server, port) {
-                if (!port)
-                    port = 8080;
-                return '<script>(function(e){e.setAttribute("src","http://'+server+':'+port+'/target/target-script-min.js");document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"))</script>';
-            }
-        };
-
-        res.render("index", { locals: locals }, function(err, html) {
-			if (generating) {
-				console.log(html);
-				fs.writeFileSync(path.join(cwd, "index.html"), html);
-			}
-        });
-        locals.phonegap = false;
-        res.render("index", { locals: locals });
-    });
-
-    app.get("/build", function(req, res) {
-        fs.readFile("index.html", function(err, data) {
-          res.send(data);
-        });
-    });
-
-    app.listen(port, "0.0.0.0");
-
-	if (generating) {
-        console.log("Building index.html");
-        var http = require("http");
-        http.get({ host: 'localhost', path: '/', port: port }, function(res) {
-            app.close();
-        });
-        
-    } else {
-		build = (config['local'])?"local":"debug";
-        console.log("Server started on port " + port);
+    if (config.settings.useStylus) {
+      var stylus = require("stylus");
+      app.use("/ui", stylus.middleware({
+        src: cwd + '/ui',
+        dest: cwd + '/ui',
+        compile: stylusCompile
+      }));
     }
+
+    var compiledDir = cwd+"/compiled";
+    app.use("/compiled", express.compiler({ src: cwd+"/app", dest: compiledDir, enable: ['less', 'coffeescript']}));
+    app.use("/compiled", express.static(compiledDir));
+
+    app.use("/ui", express.static(cwd+"/ui"));
+    app.use("/app", express.static(cwd+"/app"));
+
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+
+  app.get("/", function(req, res) { 
+
+    locals = { 
+      debug: false,
+      phonegap: true,
+      config: config[build],
+      weinre: function(server, port) {
+        if (!port)
+          port = 8080;
+        return '<script>(function(e){e.setAttribute("src","http://'+server+':'+port+'/target/target-script-min.js");document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"))</script>';
+      }
+    };
+
+    res.render("index", { locals: locals }, function(err, html) {
+      if (generating) {
+        console.log(html);
+        fs.writeFileSync(path.join(cwd, "index.html"), html);
+      }
+    });
+    locals.phonegap = false;
+    res.render("index", { locals: locals });
+  });
+
+  app.get("/build", function(req, res) {
+    fs.readFile("index.html", function(err, data) {
+      res.send(data);
+    });
+  });
+
+  app.listen(port, "0.0.0.0");
+
+  if (generating) {
+    console.log("Building index.html");
+    var http = require("http");
+    http.get({ host: 'localhost', path: '/', port: port }, function(res) {
+      app.close();
+    });
+    
+  } else {
+    build = (config['local'])?"local":"debug";
+    console.log("Server started on port " + port);
+  }
 }
 
 
